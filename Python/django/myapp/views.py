86c510453fe91424994505e5ac9827cb70126d45
Sukhjeet Chauhan <chauhansukhjeet@gmail.com>
2024-08-21 07:06:50 +1200

    django: connecting app to template file

--- Python/django/myapp/views.py
+++ Python/django/myapp/views.py
@@ -5,4 +5,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('<h1>Hello, world. You\'re at the index.</h1>')
+    name='John'
+    return render(request,'index.html', {'name': name})
